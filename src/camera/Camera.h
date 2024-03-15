0a52ac3b583970ddc2152c019985a95c15b55526
Sunerloger <106279641+Sunerloger@users.noreply.github.com>
2024-03-15 13:29:19 +0100

    fix: first attempt

--- src/camera/Camera.h
+++ src/camera/Camera.h
@@ -10,6 +10,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/euler_angles.hpp>
+#include <glm/gtx/rotate_vector.hpp>
 
 
 
@@ -18,15 +19,14 @@ public:
     glm::vec3 position;
     float yaw;
     float pitch;
-    glm::vec3 up;
+    const glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
     glm::mat4 viewMatrix;
     glm::mat4 projMatrix;
 
     Camera(float fov, float aspectRatio, float nearPlane, float farPlane) {
         position = glm::vec3(0.0f, 0.0f, 0.0f);
-        yaw = -90.0f; // Initial yaw, looking along -Z axis
+        yaw = 0.0f; // Initial yaw, looking along -Z axis
         pitch = 0.0f; // Initial pitch
-        up = glm::vec3(0.0f, -1.0f, 0.0f); // Up is -Y axis
         projMatrix = createPerspectiveProjectionMatrix(fov, aspectRatio, nearPlane, farPlane);
         updateCameraVectors();
     }
@@ -46,13 +46,7 @@ public:
     }
 
     void updateCameraVectors() {
-        glm::vec3 front;
-        front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-        front.y = sin(glm::radians(pitch));
-        front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-        front = glm::normalize(front);
-
-        viewMatrix = glm::lookAt(position, position + front, up);
+        viewMatrix = glm::lookAt(position, position + getFront(), up); // TODO maybe delete
     }
 
     glm::mat4 getViewProjMatrix() const {
@@ -64,47 +58,34 @@ public:
     }
 
     void moveForward(float delta) {
-        glm::vec3 front;
-        front.x = cos(glm::radians(yaw)) * cos(glm::radians(0.0f)); // Assuming pitch does not affect forward movement
-        front.z = sin(glm::radians(yaw)) * cos(glm::radians(0.0f));
-        front = glm::normalize(front);
-
-        position += front * delta;
+        position += getFront() * delta;
         updateCameraVectors();
     }
 
     void moveBackward(float delta) {
-        glm::vec3 front;
-        front.x = cos(glm::radians(yaw)) * cos(glm::radians(0.0f)); // Assuming pitch does not affect forward movement
-        front.z = sin(glm::radians(yaw)) * cos(glm::radians(0.0f));
-        front = glm::normalize(front);
-
-        position -= front * delta;
+        position -= getFront() * delta;
         updateCameraVectors();
     }
 
     void moveLeft(float delta) {
-        glm::vec3 front;
-        front.x = cos(glm::radians(yaw)) * cos(glm::radians(0.0f));
-        front.z = sin(glm::radians(yaw)) * cos(glm::radians(0.0f));
-        front = glm::normalize(front);
-
-        glm::vec3 right = glm::normalize(glm::cross(front, up));
+        glm::vec3 right = glm::normalize(glm::cross(getFront(), up));
 
         position -= right * delta;
         updateCameraVectors();
     }
 
     void moveRight(float delta) {
-        glm::vec3 front;
-        front.x = cos(glm::radians(yaw)) * cos(glm::radians(0.0f));
-        front.z = sin(glm::radians(yaw)) * cos(glm::radians(0.0f));
-        front = glm::normalize(front);
-
-        glm::vec3 right = glm::normalize(glm::cross(front, up));
+        glm::vec3 right = glm::normalize(glm::cross(getFront(), up));
 
         position += right * delta;
         updateCameraVectors();
+    }
+
+    glm::vec3 getFront() {
+        glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f);
+        glm::rotate(front, yaw, up);
+        glm::rotate(front, pitch, glm::vec3(1.0f, 0.0f, 0.0f));
+        return front;
     }
 
 };
