7853ab50fea56e67c66089256c950b169c42395f
Sunerloger <106279641+Sunerloger@users.noreply.github.com>
2024-04-05 11:59:46 +0200

    feat: viewMatrix and projMatrix private now

--- src/camera/CharacterCamera.h
+++ src/camera/CharacterCamera.h
@@ -10,13 +10,15 @@ using namespace JPH;
 
 class CharacterCamera {
 public:
-    glm::mat4 viewMatrix;
-    glm::mat4 projMatrix;
-
+    
     CharacterCamera(float fov, float aspectRatio, float nearPlane, float farPlane);
     virtual ~CharacterCamera();
 
     glm::mat4 getViewProjMatrix();
-
     void setViewMatrix(RMat44 physicsViewMatrix);
+
+private:
+
+    glm::mat4 viewMatrix;
+    glm::mat4 projMatrix;
 };
