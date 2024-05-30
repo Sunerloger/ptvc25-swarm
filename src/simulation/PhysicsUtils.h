325c4543e2d7328f7a599c2bd13fcf78a630285d
Sunerloger <106279641+Sunerloger@users.noreply.github.com>
2024-05-30 20:55:20 +0200

    fix: cyclic dependency in PhysicsUtils #1

--- src/simulation/PhysicsUtils.h
+++ src/simulation/PhysicsUtils.h
@@ -10,8 +10,6 @@
 
 #include <glm/glm.hpp>
 
-#include "../SceneManager.h"
-
 // Layer that objects can be in, determines which other objects it can collide with
 // Typically you at least want to have 1 layer for moving bodies and 1 layer for static bodies, but you can have more
 // layers if you want. E.g. you could have a layer for high detail collision (which is not used by the physics simulation
