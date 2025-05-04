7ecbf78230d02a75df4967bdb99258502b39582f
Sunerloger <106279641+Sunerloger@users.noreply.github.com>
2025-05-04 16:39:37 +0200

    chore: delete scripts

--- scripts/download_assets_and_libs.bat
+++
@@ -1,3 +0,0 @@
-git clone https://submission.cg.tuwien.ac.at/gcg-2023/data.git
-xcopy /E /I /Y data\* ..\
-rmdir /S /Q data\
--- scripts/download_assets_and_libs.sh
+++
@@ -1,3 +0,0 @@
-git clone https://submission.cg.tuwien.ac.at/gcg-2023/data.git
-cp -r ./data/* ../
-rm -r data/*
